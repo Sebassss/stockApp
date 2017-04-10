75b9ffa21547f3797ecc5dcb0dee7689ceb3f1b3
Sebassss <pseba20@gmail.com>
2017-04-10 12:24:04 -0300

    laburo commit

--- app/data/class.generamenu.php
+++ app/data/class.generamenu.php
@@ -57,7 +57,8 @@ class Menu extends MySQL
                             </li>';
                     }
                     else {
-                        echo '<li><a href="#">' . $resultados['menu_titulo'] . '</a></li>'; //'<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                        echo '<li>'.$resultados['Count'].'<a href="#">' . $resultados['menu_titulo'] . '</a></li>'; //'<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                        echo $resultados['Count'];
                     }
                 }
                 else
@@ -75,26 +76,36 @@ class Menu extends MySQL
 
         $result = "SELECT a.menu_id, a.menu_titulo,a.menu_icon, a.menu_link,a.menu_parent, Deriv1.Count as Count FROM menu a
                 LEFT OUTER JOIN (SELECT menu_parent, COUNT(*) AS Count FROM menu GROUP BY menu_parent) Deriv1 ON a.menu_id = Deriv1.menu_parent
-                where menu_parent = '.$parent.' order by menu_orden asc";
+                where menu_id = '.$parent.' order by menu_orden asc";
         if ($this->num_rows($result) > 0)
         {
             while ($resultados = $this->fetch_array($result))
             {
-                if($resultados['Count'] != '')
+                if ($resultados['menu_parent'] == 0)
                 {
-                    echo '<li class="dropdown-submenu">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $resultados['menu_titulo'] . ' <span class="caret"></span></a>
-                            <ul class="dropdown-menu">';
-                    //echo '<li><a href="#">' . $resultados['menu_titulo'] . '</a></li>';
-                    $this->Draw_Parents($resultados['menu_id']);
-                    echo '</ul>
-                            </li>';
+                    if ($resultados['Count'] != '')
+                    {
+                        echo '<li class="dropdown-submenu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $resultados['menu_titulo'] . ' <span class="caret"></span></a>
+                                <ul class="dropdown-menu">';
+                        //echo '<li><a href="#">' . $resultados['menu_titulo'] . '</a></li>';
+                        $this->Draw_Parents($resultados['menu_id']);
+
+                    }
+                    else
+                    {
+                        $this->Draw_Parents($resultados['menu_id']);
+                        //echo '<li><a href="#">' . $resultados['menu_titulo'] . '</a></li>';
+                        echo $resultados['Count'];
+                    }
+
                 }
                 else
                 {
+                    echo '<li><a href="#">'.$resultados['menu_titulo'] . '</a></li>'; //'<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
 
-                    echo '<li><a href="#">' . $resultados['menu_titulo'] . '</a></li>';
                 }
+
             }
 
         }
