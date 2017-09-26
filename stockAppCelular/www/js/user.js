90f76d2cac5a64ca69a7b8228e6df223e6e611c6
ivanneira <ien1983@gmail.com>
2017-09-26 08:56:08 -0300

    agrego app de celular en el mismo repositorio

---
+++ stockAppCelular/www/js/user.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Ivan on 26/09/2017.
+ */
+/**
+ * Created by Ivan on 25/09/2017.
+ */
+
+var myApp = new Framework7();
+
+var $$ = Dom7;
+
+var articulos;
+
+$(function(){
+
+    myApp.showPreloader('Espere...');
+
+    var mySearchbar = $$('.searchbar')[0].f7Searchbar;
+
+    $.ajax({
+        url: 'http://10.64.65.200:84/stockapp/public/abm_getArticulos',
+        type: "GET",
+        dataType: "json",
+        success: function(data)
+        {
+            //console.dir(data);
+
+            fillTable(data);
+            articulos = data;
+
+            myApp.hidePreloader();
+        },
+        error: function(error)
+        {
+            console.log(error);
+            myApp.hidePreloader();
+        }
+
+    });
+
+});
+
+function fillTable(data){
+
+
+    console.dir(data);
+
+    for ( var index in data){
+
+        var htmlBadge;
+
+        if(data[index].articulo_cantidad <= 1){
+            htmlBadge = '<span class="badge  bg-red">'+ data[index].articulo_cantidad +'</span>';
+        }else{
+            htmlBadge = '<span class="badge  bg-green">'+ data[index].articulo_cantidad +'</span>';
+        }
+
+        var html =
+            '<li class="item-content">'+
+            '<div class="item-inner">'+
+            htmlBadge +
+            '<div class="item-title">'+ data[index].articulo_nombre + '<br>'+
+
+            '<div class="chip  bg-lightgreen"><div class="chip-label ">'+ data[index].deposito_nombre+ '</div></div>' +
+
+            '</div>'+
+            '</div>'+
+            '</li>';
+
+
+        $("#listContent").append(html);
+
+    }
+
+}
