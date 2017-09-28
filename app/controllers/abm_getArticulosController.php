1606262d4996730a69471c571673309cc408b61a
Sebassss <pseba20@gmail.com>
2017-09-28 08:31:24 -0300

    .

--- app/controllers/abm_getArticulosController.php
+++ app/controllers/abm_getArticulosController.php
@@ -100,13 +100,13 @@ function descArticulos()
     $b = $_POST['usuario_id'];
     $c = $_POST['cantidad'];
 
-    $result_rows = $db->Consulta("select articulo_cantidad from articulos where articulo_id='".$a."'");
+    $result_rows = $db->Consulta("select articulo_cantidad from articulos where articulo_id=".$a);
 
     $mensaje = "No pudo guardar.";
     $estado = "false";
 
     if($db->num_rows($result_rows)>0) {
-        $result = $db->Consulta("update articulos set articulo_cantidad='".$c."' where articulo_id='".$a."'");
+        $result = $db->Consulta("update articulos set articulo_cantidad=".$c." where articulo_id=".$a);
 
         if (!$result) {
             $mensaje = "Procesado correctamente.";
